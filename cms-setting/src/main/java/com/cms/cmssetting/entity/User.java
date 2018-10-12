f6f7110c37454d1f3c0c35e61ca99e4e93437ede
MSCLOUD <826256830@qq.com>
2018-10-12 20:06:44 +0800

    mybatis

---
+++ cms-setting/src/main/java/com/cms/cmssetting/entity/User.java
@@ -0,0 +1,43 @@
+package com.cms.cmssetting.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+* Created by Momf Generator on 2018/10/12
+*/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private Integer age;
+
+    /** 
+    * 密码
+    */
+    private String password;
+
+    /** 
+    * 加密密码的盐
+    */
+    private String salt;
+
+    /** 
+    * 用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
+    */
+    private Integer state;
+
+    /** 
+    * 是否可用
+    */
+    private Boolean available;
+}
---
+++ cms-setting/src/main/java/com/cms/cmssetting/mapper/UserMapper.java
@@ -0,0 +1,25 @@
+package com.cms.cmssetting.mapper;
+
+import com.cms.cmssetting.entity.User;
+import com.cms.cmssetting.entity.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+/**
+* Created by Momf Generator on 2018/10/12
+*/
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
