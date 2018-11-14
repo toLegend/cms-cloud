de3da048543b037cb31cf4239e01d3326e20bb22
MSCLOUD <826256830@qq.com>
2018-11-14 16:17:43 +0800

    oauth2

--- cms-setting/src/main/java/com/cms/cmssetting/CmsSettingApplication.java
+++ cms-setting/src/main/java/com/cms/cmssetting/CmsSettingApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * @author momf
@@ -16,6 +18,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableDiscoveryClient
 @MapperScan("com.cms.cmssetting")
+@EnableResourceServer
+@EnableAuthorizationServer
 public class CmsSettingApplication {
 
     public static void main(String[] args) {
